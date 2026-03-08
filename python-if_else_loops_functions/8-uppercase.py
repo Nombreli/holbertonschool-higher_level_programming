89d506642f97b65ce61f7950afdf81ec41e6fdd2
Nombreli <13177@holbertonstudents.com>
2026-03-08 11:02:47 +0000

    Add task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
         if 97 <= ord(char) <= 122:
-            print(chr(ord(char) - 32), end="")
+            result += chr(ord(char) - 32)
         else:
-            print(char, end="")
-    print("")
+            result += char
+    print("{}".format(result))
