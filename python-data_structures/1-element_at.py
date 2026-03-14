580fceaedfda087893448474124c7015c2cb2c5e
Nombreli <13177@holbertonstudents.com>
2026-03-14 12:49:47 +0000

    Fix pycodestyle whitespace error

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    # İndeks mənfidirsə və ya siyahının ölçüsündən böyükdürsə None qaytar
     if idx < 0 or idx >= len(my_list):
         return None
-    
-    # Əgər indeks düzgündürsə, həmin elementi qaytar
     return my_list[idx]
