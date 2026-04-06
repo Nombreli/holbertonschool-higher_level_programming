f70c2d8f67fe86e881eefc771d844689ee4745d2
Nombreli <13177@holbertonstudents.com>
2026-04-06 20:26:53 +0000

    Add list_division function and test

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """Divide element by element two lists.
+
+    Args:
+        my_list_1 (list): birinci siyahı
+        my_list_2 (list): ikinci siyahı
+        list_length (int): nəticə siyahısının uzunluğu
+
+    Returns:
+        list: bölmələrin nəticələri
+    """
+    new_list = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except (TypeError, ValueError):
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new_list.append(result)
+    return new_list
