79d46e6f04405bbad7fdeaef94e62bdd65641f5d
Nombreli <13177@holbertonstudents.com>
2026-04-07 10:14:55 +0000

    Add lookup function and test file

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module provides a function to return
+the list of attributes and methods of an object.
+"""
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        list: A list of attribute and method names.
+    """
+    return dir(obj)
---
+++ python-inheritance/README.md
@@ -0,0 +1,5 @@
+# Python Inheritance Project
+
+This project covers Python inheritance concepts.
+Task 0 implements a function `lookup(obj)` that returns
+the list of attributes and methods of an object.
