bad290b1b39b77b7e05f8134eddd8c0b2c38c67d
Nombreli <13177@holbertonstudents.com>
2026-04-07 13:19:24 +0000

    Fix: Add all missing edge cases to pass the checker

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""Module for BaseGeometry class."""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """A class with public instance methods."""
+    """Represent base geometry."""
 
     def area(self):
-        """Raises an Exception with a message."""
+        """Not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value as an integer."""
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
