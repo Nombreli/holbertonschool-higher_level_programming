f15ea738abe043aa392c4293c262a446d1d41ccb
Nombreli <13177@holbertonstudents.com>
2026-05-05 19:49:46 +0200

    Read file function

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Reads a text file (UTF-8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Reads a file and prints its content"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
