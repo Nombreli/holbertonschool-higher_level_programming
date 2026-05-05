5d97c0550676d2dd10bb08040e437ae3bcd06194
Nombreli <13177@holbertonstudents.com>
2026-05-05 20:21:09 +0200

    Pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Returns Pascal's triangle"""
+
+
+def pascal_triangle(n):
+    """Build Pascal triangle"""
+
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+
+        if triangle:
+            last_row = triangle[-1]
+
+            for j in range(len(last_row) - 1):
+                row.append(last_row[j] + last_row[j + 1])
+
+            row.append(1)
+
+        triangle.append(row)
+
+    return triangle
