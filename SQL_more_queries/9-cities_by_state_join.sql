0f532d2ed5fc89eecb80e294d102a32512c21bd2
Nombreli <13177@holbertonstudents.com>
2026-05-05 23:48:23 +0200

    Task 9: cities by states using JOIN

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- Lists cities with their state names using JOIN
+SELECT
+    cities.id,
+    cities.name,
+    states.name
+FROM cities
+JOIN states
+    ON cities.state_id = states.id
+ORDER BY cities.id ASC;
